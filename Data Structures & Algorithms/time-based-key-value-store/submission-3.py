8fd95517a3fd8b7f9e0ed319eec956764f98c1f1
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2026-07-18 16:37:07 -0700

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,23 @@
+class TimeMap:
+    def __init__(self):
+        self.dic = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.dic[key].append((-timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.dic:
+            return ""
+        heap = self.dic[key].copy()
+        heapq.heapify(heap)
+
+        for _ in range(len(heap)):
+            time, val = heapq.heappop(heap)
+            if -1*time <= timestamp:
+                return val
+        return ""
+        
+            
+                
+
+        
